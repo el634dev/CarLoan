894a6f22d3ff1a8f9c8b2d1387df09f20852ec95
el634dev <al7758066@gmail.com>
2026-02-24 11:56:22 -0500

    Added a slider to adjust the annual interest rate

--- app/src/main/java/com/example/carloan/MainActivity.kt
+++ app/src/main/java/com/example/carloan/MainActivity.kt
@@ -1,28 +1,36 @@
 package com.example.carloan
 
 import android.os.Bundle
-//import android.widget.RadioGroup
+import android.widget.RadioGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.TextField
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.ui.Alignment
-//import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +54,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CarLoanScreen(modifier: Modifier = Modifier) {
+    var loanNum by remember { mutableStateOf("") }
+    var interestRate by remember { mutableFloatStateOf(0.00f) }
+
     Column(
         modifier = modifier
     ) {
@@ -53,18 +64,59 @@ fun CarLoanScreen(modifier: Modifier = Modifier) {
             text = "Car Loan Calculator",
             fontSize = 20.sp
         )
-        RadioGroup(
-            radioOptions = listOf("10", "20", "30")
+        TextField(
+            value = loanNum,
+            singleLine = true,
+            label = {
+                Text(text = "Car Purchase Price: ")
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            onValueChange = {
+                loanNum = it
+            },
+            modifier = Modifier.padding(bottom = 16.dp)
         )
+        RadioGroup(
+            radioOptions = listOf(10, 20, 30)
+        )
+        InterestSlider( interestVal = interestRate, onChange = { interestRate = it } )
+        Button(
+            onClick = {},
+            modifier = modifier
+        ) {
+            Text("Calculate")
+        }
     }
+}
+
+// -----------------------------------------
+// Slider to adjust the annual interest rate
+@Composable
+fun InterestSlider(interestVal: Float, onChange: (Float)->Unit) {
+    Slider(
+        value = interestVal,
+        onValueChange = {
+            onChange(it)
+        },
+        valueRange = 0f .. 30f,
+        modifier = Modifier.padding(8.dp)
+    )
+    Text(
+        text = interestVal.toInt().toString(),
+        textAlign = TextAlign.Center,
+        fontSize = 20.sp,
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
 // -----------------------------------------
 // Gives a list of radio buttons
 @Composable
-fun RadioGroup(radioOptions: List<String>){
+fun RadioGroup(radioOptions: List<Int>){
     // mutableIntStateOf
-    var selectedOption by remember { mutableStateOf(value = "10") }
+    var selectedOption by remember { mutableIntStateOf(10) }
 
     Column(
         modifier = Modifier
@@ -73,11 +125,13 @@ fun RadioGroup(radioOptions: List<String>){
         radioOptions.forEach{ option ->
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier.selectable(
-                    selected = selectedOption == option,
-                    onClick = { selectedOption = option },
-                    role = Role.RadioButton
-                ).padding(8.dp)
+                modifier = Modifier
+                    .selectable(
+                        selected = selectedOption == option,
+                        onClick = { selectedOption = option },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp)
             ) {
                 RadioButton(
                     selected = selectedOption == option,
